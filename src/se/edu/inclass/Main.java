c4e4fcb2c1bbe5584dda6775ccd41c8dacd88875
Zijin <e0772699@u.nus.edu>
2023-02-17 21:38:33 +0800

    Consume the new methods

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,12 +16,15 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        printData(tasksData);
-        System.out.println();
+//        printData(tasksData);
+//        printDataUsingStreams(tasksData);
+//        System.out.println();
+
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDeadlinesUsingStream(tasksData);
+        System.out.println("Total number of deadlines counted using streams: "
+                + countDeadlinesUsingStream(tasksData));
 
     }
 
