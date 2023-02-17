c147e85959da1076aceb5b6aee7d662575b169c1
Zijin <e0772699@u.nus.edu>
2023-02-17 21:37:41 +0800

    Added methods to print all tasks and print deadlines using stream

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -43,16 +43,34 @@ public class Main {
         return count;
     }
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using iteration.");
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    public static void printDataUsingStreams(ArrayList<Task> tasks){
+        System.out.println("Printing data using streams");
+        tasks.stream()  //first convert it into a stream
+                //to improve performance for large data, you can use .parallelStream
+                .forEach(System.out::println);//using sout here will resolve to a C++ way of referring to a method
+                // this forEach will print all elements in the list (appling the print method for all)
+    }
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Printing Deadlines using iterations");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream( ArrayList<Task> tasks) {
+        System.out.println("Printing Deadlines using streams");
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                // The filter takes a predicate to filter through the data
+                // Predicate: t instanceof Deadline, which is a method
+                .forEach(System.out::println);
     }
 }
